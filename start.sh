a7616ed30518744d7a888e3dd3045d2d4be0c1e3
Dann Bohn <djb44@psu.edu>
2019-10-09 15:07:35 -0400

    bundle exec in start script

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ ${APP_ROLE:-app} == "sidekiq" ]; then
     bundle exec sidekiq
 else
     echo "starting rails"
-    rails db:create
-    rails db:migrate
-    rails s -b '0.0.0.0'
+    bundle exec rails db:create
+    bundle exec rails db:migrate
+    bundle exec rails s -b '0.0.0.0'
 fi
