c7cbf00024db611d5399886e796a981da69f31b7
Dann Bohn <djb44@psu.edu>
2019-10-09 15:06:54 -0400

    parameterize ldap bits (wip)

--- app/controllers/ldap_controller.rb
+++ app/controllers/ldap_controller.rb
@@ -3,7 +3,7 @@ require "net/ldap"
 class LdapController < ApplicationController
   def initialize
     @ldap = Net::LDAP.new
-    @ldap.host = "dirapps.aset.psu.edu"
+    @ldap.host = Rails.configuration.ldap_server
     @ldap.bind
     @attributes = Hash.new
   end
@@ -11,14 +11,14 @@ class LdapController < ApplicationController
   def find(access_id)
     attributes = Hash.new
     filter = "uid=#{access_id}"
-    base = "dc=psu,dc=edu"
+    base = Rails.configuration.ldap_base
     results = @ldap.search(base: base, filter: filter)
     results[0]
   end
 
   def ldap_attributes(access_id)
     # TODO parameterize a lot of the LDAP stuff
-    admin_umg = "cn=umg/up.ul.dsrd.sudoers,dc=psu,dc=edu"
+    admin_umg = Rails.configuration.admin_umg
     result = find(access_id)
     @attributes[:last_name] = result[:sn][0]
     @attributes[:first_name] = result[:givenname][0]
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module PsuAuthproxy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.ldap_server = 'dirapps.aset.psu.edu'
+    config.ldap_base = "dc=psu,dc=edu"
+    config.admin_umg = "cn=umg/up.ul.dsrd.sudoers,dc=psu,dc=edu"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
