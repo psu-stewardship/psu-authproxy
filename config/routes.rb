0e145611b4351081c52b45b29eb8023a3a7602f1
Dann Bohn <djb44@psu.edu>
2019-12-16 11:43:52 -0500

    use oidc routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  use_doorkeeper_openid_connect
   use_doorkeeper
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
